7747197b6a0cc55e5de3c960b129202f2c70e7c6
jikim127 <jikim127@gmail.com>
2023-08-24 17:51:41 +0900

    PlayList쪽 update 4차

--- src/main/java/com/example/vibemusic/controller/PlayListController.java
+++ src/main/java/com/example/vibemusic/controller/PlayListController.java
@@ -1,13 +1,21 @@
 package com.example.vibemusic.controller;
 
+import com.example.vibemusic.domain.Music;
+import com.example.vibemusic.domain.PlayList;
+import com.example.vibemusic.dto.MusicDTO;
 import com.example.vibemusic.dto.NewsDTO;
+import com.example.vibemusic.dto.PageRequestDTO;
+import com.example.vibemusic.dto.PageResponseDTO;
+import com.example.vibemusic.service.MusicService;
 import com.example.vibemusic.service.PlayListService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping
@@ -16,9 +24,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PlayListController {
 
     private final PlayListService playListService;
+    private final MusicService musicService;
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/playlist")
-    public void main(){
-
+    public String addList(@RequestParam String plName){
+        playListService.addPlaylist(plName);
+        return "redirect:/playlist"; // 리스트 페이지로 리다이렉트
     }
 }
--- src/main/java/com/example/vibemusic/service/PlayListService.java
+++ src/main/java/com/example/vibemusic/service/PlayListService.java
@@ -1,4 +1,9 @@
 package com.example.vibemusic.service;
 
+import com.example.vibemusic.dto.MusicDTO;
+import com.example.vibemusic.dto.PageResponseDTO;
+
 public interface PlayListService {
+
+    void addPlaylist(String plName);
 }
