07ce47de2abfb9a502c05cb1feeba3c3658e237a
kimsohwi <berry1699@naver.com>
2023-09-05 16:49:39 +0900

    사이드 메뉴 삭제

--- src/main/resources/static/js/questionMod.js
+++ src/main/resources/static/js/questionMod.js
@@ -1,37 +1,4 @@
-
-    const errors =[[${errors}]]
-    console.log(errors)
-
-    let errorMsg = ''
-
-    if(errors){
-    for (let i = 0; i< errors.length; i++){
-    errorMsg += `${errors[i].field}은(는) ${errors[i].code} \n`
+async function getQuestion(qno){
+    const response = await axios.get(`/questionMod/${qno}`)
+    return response.data
 }
-    history.replaceState({}, null, null)
-    alert(errorMsg)
-
-}
-
-
-    //modify
-    const link = [[${pageRequestDTO.getLink()}]]
-    const formObj = document.querySelector("#f1")
-
-    document.querySelector(".modBtn").addEventListener("click", function (e){
-    e.preventDefault()
-    e.stopPropagation()
-
-    formObj.action = `/questionModify?${link}`
-    formObj.method = 'post'
-    formObj.submit()
-},false)
-
-    document.querySelector(".removeBtn").addEventListener("click",function (e){
-    e.preventDefault()
-    e.stopPropagation()
-
-    formObj.action = `/questionRemove`
-    formObj.method = 'post'
-    formObj.submit()
-},false)
